81dfe281c046d4763eeb543e5c07b935204a5485
Viictor-gulielmitti <79719548+Viictor-gulielmitti@users.noreply.github.com>
2021-03-06 17:49:11 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************************");
+            Console.WriteLine("Cidades do Litoral Sul:");
+            Console.WriteLine("**************************");
+            Console.WriteLine("Cananéia");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Iguape");
+            Console.ForegroundColor = ConsoleColor.Red;            
+            Console.WriteLine ("Ilha Comprida");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine ("Itanhaém");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Mongaguá");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Peruíbe");
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
+    }
+}
